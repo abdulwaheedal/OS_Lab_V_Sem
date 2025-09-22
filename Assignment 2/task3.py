f1d16d3665bed03d4b889a83ff6c8110829dcb52
Faaiz <135386027+abdulwaheedal@users.noreply.github.com>
2025-09-22 12:04:54 +0530

    Update task3.py

--- Assignment 2/task3.py
+++ Assignment 2/task3.py
@@ -3,10 +3,8 @@ import multiprocessing
 if __name__ == '__main__':
     print("System Starting...")
 
-    # Create two processes
     p1 = multiprocessing.Process(target=system_process, args=('Process-1',))
     p2 = multiprocessing.Process(target=system_process, args=('Process-2',))
 
-    # Start both processes
     p1.start()
     p2.start()
